f16133a3f030a22a41afa7f64c5c2a9564190f7d
JihyoPark <seoulit77a@gmail.com>
2024-02-19 21:01:30 +0900

    Nuxt Front File

--- .nuxt/types/auth.d.ts
+++
@@ -1,6 +0,0 @@
-declare module '#auth' {
-  const getServerSession: typeof import('C:/Users/Jihyo Park/Desktop/logi77_frontend/node_modules/.pnpm/@sidebase+nuxt-auth@0.6.0-rc.0_next-auth@4.21.1_vue@3.3.4/node_modules/@sidebase/nuxt-auth/dist/runtime/server/services').getServerSession
-  const getToken: typeof import('C:/Users/Jihyo Park/Desktop/logi77_frontend/node_modules/.pnpm/@sidebase+nuxt-auth@0.6.0-rc.0_next-auth@4.21.1_vue@3.3.4/node_modules/@sidebase/nuxt-auth/dist/runtime/server/services').getToken
-  const NuxtAuthHandler: typeof import('C:/Users/Jihyo Park/Desktop/logi77_frontend/node_modules/.pnpm/@sidebase+nuxt-auth@0.6.0-rc.0_next-auth@4.21.1_vue@3.3.4/node_modules/@sidebase/nuxt-auth/dist/runtime/server/services').NuxtAuthHandler
-
-}
